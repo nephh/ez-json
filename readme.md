90a7fa71c7278b6dbc979623a59d41df9d641378
nephh <owenmccr@gmail.com>
2024-01-06 20:07:19 -0500

    readme and filename changes

--- readme.md
+++ readme.md
@@ -1,10 +1,13 @@
+<div align="center">
+
 # EZ-JSON CLI
 
 ![GitHub License](https://img.shields.io/github/license/nephh/ez-json?style=for-the-badge&color=blue)
 
-## Description
 
 EZ-JSON is a command-line interface (CLI) tool for generating JSON data based on user input. It's useful for quickly creating mock data for testing or development purposes. Whether you need to quickly test a post endpoint, or you need a huge seed file, EZ-JSON makes it simple and easy.
+
+</div>
 
 ## Table of Contents
 
--- utils/json.js
+++ utils/json.js
@@ -3,8 +3,16 @@ import { randNumber, randString, randDictionaries } from "./randomGenerator.js";
 
 function handleString(key, value) {
   const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  const firstNames = ["firstname", "first-name", "first_name"];
+  const lastNames = ["lastname", "last-name", "last_name"];
+
   if (key === "name") {
-    return [key, uniqueNamesGenerator({ dictionaries: [names] })];
+    return [
+      key,
+      `${uniqueNamesGenerator({
+        dictionaries: [names],
+      })} ${uniqueNamesGenerator({ dictionaries: [names] })}`,
+    ];
   } else if (key === "username") {
     return [
       key,
@@ -21,6 +29,13 @@ function handleString(key, value) {
       `${randNumber(10, 4999)} ${uniqueNamesGenerator({
         dictionaries: [names],
       })} St.`,
+    ];
+  } else if (firstNames.includes(key) || lastNames.includes(key)) {
+    return [
+      key,
+      uniqueNamesGenerator({
+        dictionaries: [names],
+      }),
     ];
   } else if (regex.test(value)) {
     return [
